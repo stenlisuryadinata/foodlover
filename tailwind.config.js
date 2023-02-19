75fcf2a114c606123f60eaa363690ba753bd9c66
Stenli Suryadinata <47641964+stenlisuryadinata@users.noreply.github.com>
2023-02-19 21:18:39 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 //   content: ["./src/**/*.{html,js}"],
-  content: [
-		'./pages/**/*.{js,ts,jsx,tsx}',
-		'./src/components/**/*.{js,ts,jsx,tsx}',
-	],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
